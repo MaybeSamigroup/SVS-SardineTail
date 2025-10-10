81cfbcc19a238d19b893a2c880e684098c1e3ac9
MaybeSemigroup <higeagito@yahoo.co.jp>
2025-10-11 01:56:23 +0900

    Update plugin version.

--- SardineTail.cs
+++ SardineTail.cs
@@ -515,7 +515,7 @@ namespace SardineTail
     public partial class Plugin : BasePlugin
     {
         public const string Name = "SardineTail";
-        public const string Version = "2.1.6";
+        public const string Version = "2.1.7";
         public const string Guid = $"{Process}.{Name}";
         internal const string AssetBundle = "sardinetail.unity3d";
         internal static Plugin Instance;
